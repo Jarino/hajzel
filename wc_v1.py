34946df47a7a7b9859c973913e9741099bad8137
Michal Janosik <miso.janosik@gmail.com>
2017-02-17 00:16:11 +0100

    pridanie nahodneho vyberu

--- wc_v1.py
+++ wc_v1.py
@@ -1,7 +1,9 @@
 import pygame
+from __builtin__ import open
 import RPi.GPIO as GPIO
 import time
 import os
+from random import randint
 
 buttonPin = 40
 GPIO.setmode(GPIO.BOARD)
@@ -9,73 +11,107 @@ GPIO.setup(buttonPin,GPIO.IN)
 
 pygame.init()
 pygame.mixer.init()
-a=0
-b=0
+
+
 c=0
-d=0
-x=0
+close=0
+open=0
+open1=0
+close1=0
+
+def randomize():
+    string1 = str(randint(2,6))
+    string = string1 + ".mp3"
+    return string
+
+def play_rand(c):
+    nazov=randomize()
+    pygame.mixer.music.load(nazov)
+    time.sleep(1)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy() == True:
+        if (GPIO.input(buttonPin) == False):  # zistujeme ci su dvere otvorene. ak su dvere otvorene v kazdom cykle sa pripocita
+            open1 = 0
+        elif (GPIO.input(buttonPin) == True):
+            open1 += 1
+        if open1<5:
+            close1=0
+            while close1 < 4:
+                if (GPIO.input(buttonPin) == False):
+                    close1 += 1
+                elif (GPIO.input(buttonPin) == True):
+                    close1 = 0
+            if close1 == 3:
+                time.sleep(2)
+                c+= 1
+                quit();
+    open1=0
+    close1=0
+    return c
+
 def quit():
-         pygame.mixer.music.stop()
-#        print 'quit'
+    pygame.mixer.music.stop()
+    time.sleep(2)
 
 def play(c):
-        pygame.mixer.music.load("1.mp3")
-        time.sleep(3)
-        pygame.mixer.music.play()
-        before=GPIO.input(buttonPin)
-        time.sleep(3)
-        while pygame.mixer.music.get_busy()==True:
-                if (GPIO.input(buttonPin)==False):
-                        b=0
-                if (GPIO.input(buttonPin)==True):
-                        b+=1
-                if b==5:
-#                       print 'quit'
-                        time.sleep(5)
-#                       print 'open'
-#                       print before
-                        c+=1
-                        quit();
-                        time.sleep(5)
-        a=0
-        b=0
-        return c
+    pygame.mixer.music.load("1.mp3")
+    time.sleep(1)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy() == True:
+        if (GPIO.input(buttonPin) == False):  # zistujeme ci su dvere otvorene. ak su dvere otvorene v kazdom cykle sa pripocita
+            open1 = 0
+        elif (GPIO.input(buttonPin) == True):
+            open1 += 1
+        if open1<5:
+            close1=0
+            while close1 < 4:
+                if (GPIO.input(buttonPin) == False):
+                    close1 += 1
+                elif (GPIO.input(buttonPin) == True):
+                    close1 = 0
+            if close1 == 3:
+                time.sleep(2)
+                c+= 1
+                quit();
+    open1=0
+    close1=0
+    return c
 
-#       if pygame.mixer.music.get_busy()==False:
-#               quit()
+
 
 
 if __name__ == "__main__":
         while True:
-#                print 'binit'
-#                print GPIO.input(buttonPin)
-                time.sleep(0.5)
-                if (GPIO.input(buttonPin)==False):
-                        a=0
-                if (GPIO.input(buttonPin)==True):
-                        a+=1
-                if a==3:
-                        c+=1
-#                       print c
-                        pomocna=play(c);
-#                       c+=pomocna
-#                       print pomocna
-#                       print 'pred' + c
-                        while pomocna<2:
-                                if (GPIO.input(buttonPin)==False):
-                                        d=0
-                                if (GPIO.input(buttonPin)==True):
-                                        d+=1
-                                if d==3:
-                                        pomocna+=1
-#                                       print pomocna
-#                                       print 'in loop'
-                                        time.sleep(7)
-#                       print 'out loop'
-                pomocna=0
-                c=0
-                d=0
+            time.sleep(0.3)
+            if (GPIO.input(buttonPin) == False):    #zistujeme ci su dvere otvorene. ak su dvere otvorene v kazdom cykle sa pripocita
+                open = 0                            #1 Hlavny cyklus kontroly otvorenia trva 0.3 sekundy
+            elif (GPIO.input(buttonPin) == True):
+                open+= 1
+            if open== 3:
+                close=0
+                while close<4:
+                    if (GPIO.input(buttonPin) == False):
+                        close+=1
+                    elif (GPIO.input(buttonPin) == True):
+                        close=0
+                if close<5:
+                    c+=1
+                    prehrane=play(c)
+                    if prehrane==1:
+                        prehrane=play_rand(c);
 
-
-
-
+                    while prehrane < 2:
+                        if (GPIO.input(buttonPin) == False):
+                            d = 0
+                        if (GPIO.input(buttonPin) == True):
+                            d += 1
+                        if d == 3:
+                            prehrane += 1
+                            time.sleep(7)
+        a=0
+        prehrane=0
+        d=0
+        c=0
+        open=0
+        close=0
+#TODO uploadni projekt do hajzla
